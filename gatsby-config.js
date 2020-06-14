562ab514db0bd8f40eca4ba41059628497fcb495
Luiz Pedro <luizpsmoreira@gmail.com>
2020-06-14 09:42:38 -0300

    add config pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,17 +26,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Compartilhando Cargas`,
+        short_name: `CC`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#075E54`,
+        theme_color: `#075E54`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
